b23d0f98db218aaeb7e49b9da8fc9c5289974b6e
Kenneth Nturibi <knturibi@fb.com>
2018-07-12 12:03:44 -0700

    Add all files

---
+++ app/src/main/java/com/knturibifb/coolandroidloginscreen/MainActivity.java
@@ -0,0 +1,19 @@
+package com.knturibifb.coolandroidloginscreen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+   public static void onClickLogin (View view){
+
+
+    }
+}
